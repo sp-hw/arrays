5a66c18775095d8febc9cffad69780193b3201ff
Artem Petrosyan <artem.petrosyan@magenta-technology.com>
2023-02-24 19:58:14 +0100

    problems.txt

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    // Объявить метод «сгенерироватьМассив»
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
 }
